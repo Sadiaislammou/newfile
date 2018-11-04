3205b25b17092ea0cf439689e098468ce2e42405
Sadia Islam Mou <41648473+Sadiaislammou@users.noreply.github.com>
2018-11-04 22:39:46 +0600

    Add files via upload

---
+++ js/active.js
@@ -0,0 +1,11 @@
+$("a.link").click(function(){
+      $("a.link").css({"background-color": "#fff", "color": "#000"});
+    $(this).css({"background-color": "#9476C9", "border-radius": "5px","color": "#fff"});
+});
+
+
+
+$("a.link1").click(function(){
+      $("a.link1").css({"background-color": "#fff", "color": "#65696E", "font-weight": "normal"});
+    $(this).css({"background-color": "#F7F7F7", "font-weight": "bold","color": "#65696E"});
+});
